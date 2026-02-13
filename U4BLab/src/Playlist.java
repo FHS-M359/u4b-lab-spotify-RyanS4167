48a8f58ce38002c15132bf91cfd256b8e8d26b12
RyanS4167 <ryanjayden223@gmail.com>
2026-02-13 09:58:27 -0600

    secondCommit spotify

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Playlist {
+    private ArrayList<Song> songs;
+    public Playlist(ArrayList<Song> songs){
+        this.songs = songs;
+    }
+    public String toString(ArrayList<Song> rowSongs){
+        return String.format("%-15s %-13s %-15s %-6s %5s%n", "Title", "Artist", "Album", "Year", "Genre");
+    }
 }
--- U4BLab/src/Song.java
+++ U4BLab/src/Song.java
@@ -1,2 +1,77 @@
 public class Song {
+    private String title, artist, album;
+    private int durationSeconds;
+    private int releaseYear;
+    private String genre;
+    public Song(String title, String artist, String album, int seconds, int year, String genre){
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        durationSeconds = seconds;
+        releaseYear = year;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public void setDurationSeconds(int durationSeconds) {
+        this.durationSeconds = durationSeconds;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public String toString(){
+        return String.format("%-15s %-13s %-15s %-6s %5s%n", title, artist, album, releaseYear, genre);
+    }
+    public void readSongData(){
+
+    }
+    public void searchDisplayByGenre(){
+
+    }
+    public void sortByArtist(){
+
+    }
+    public void sortByYear(){
+
+    }
 }
