3d85f26624f1a2978b0d1d036b4ff8f8da176c8b
RyanS4167 <ryanjayden223@gmail.com>
2026-02-19 10:09:25 -0600

    6th committ

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 public class Playlist {
-    private ArrayList<Song> songs;
+    private static ArrayList<Song> songs;
     public Playlist(){
         songs = new ArrayList<>();
     }
@@ -20,8 +20,37 @@ public class Playlist {
         }
         return result;
     }
-    public static void searchByGenre(){
-
+    public static void readSongData() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("spotify_unique_years_artists.txt"));
+        ArrayList<Song> song = new ArrayList<Song>();
+        while(scan.hasNextLine()){
+            String line = scan.nextLine();
+            String[] parts = line.split(",");
+            String title = parts[0];
+            String artist = parts[1];
+            String album = parts[2];
+            int year = Integer.parseInt(parts[4]);
+            String genre = parts[5];
+            Song songs = new Song(title, artist, album, year, genre);
+            song.add(songs);
+        }
+        scan.close();
+        Song.printInfo(song);
+    }
+    public static void searchByGenre(String input){
+        boolean found = false;
+        for (Song s : songs){
+            if(s.getGenre().equalsIgnoreCase(input) ){
+                System.out.println(s);
+                found = true;
+            }
+            else {
+                System.out.println("No songs in this genre are found.");
+            }
+        }
+    }
+    public static void sortByAZ(){
+        
     }
 
 }
--- U4BLab/src/SpotifyTester.java
+++ U4BLab/src/SpotifyTester.java
@@ -3,28 +3,47 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 
 public class SpotifyTester {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("spotify_unique_years_artists.txt"));
-        ArrayList<Song> song = new ArrayList<Song>();
-        while(scan.hasNextLine()){
-            String line = scan.nextLine();
-            String[] parts = line.split(",");
-            String title = parts[0];
-            String artist = parts[1];
-            String album = parts[2];
-            int year = Integer.parseInt(parts[4]);
-            String genre = parts[5];
-            Song songs = new Song(title, artist, album, year, genre);
-            song.add(songs);
+        Scanner scan = new Scanner(System.in);
+        int choice = 0;
+        while(choice != 7){
+            System.out.println("===== Spotify Menu =====");
+            System.out.println(1 + " - Sort by artist(A-Z)");
+            System.out.println(2 + " - Sort by Artist(Z-A)");
+            System.out.println(3 + " - Sort by year(oldest - newest)");
+            System.out.println(4 + " - Sort by year(newest - oldest)");
+            System.out.println(5 + " - Search by Genre");
+            System.out.println(6 + " - Display all songs");
+            System.out.println(7 + " - Quit");
+            System.out.println("Choose an option: ");
+            choice = getValidOption(scan);
         }
-        scan.close();
-        Song.printInfo(song);
+
     }
+    public static int getValidOption(Scanner scan){
+        int choice = 0;
+        while(true){
+            try{
+                choice = scan.nextInt();
+                if(choice >= 1 && choice <=7){
+                    return choice;
+                }
+                else {
+                    System.out.println("Enter a number 1-7");
+                }
+            }
+            catch (InputMismatchException e){
+                System.out.println("Please enter only numbers 1-7");
+            }
+        }
+    }
+
 
 
 }
